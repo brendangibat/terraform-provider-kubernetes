33563ad55a3c863c1312b302247661a000dd40d9
Matt McCoy <mattnenterprise@yahoo.com>
2015-11-01 00:40:12 -0400

    Adding Kubernetes service support

---
+++ resource_kubernetes_service.go
@@ -0,0 +1,75 @@
+package main
+
+import "github.com/hashicorp/terraform/helper/schema"
+
+func resourceKubernetesService() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceKubernetesServiceCreate,
+		Read:   resourceKubernetesServiceRead,
+		Update: resourceKubernetesServiceUpdate,
+		Delete: resourceKubernetesServiceDelete,
+		Schema: map[string]*schema.Schema{
+			"spec": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem:     resourceUnitServiceSpec(),
+			},
+			"metadata": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem:     resourceUnitMetadata(),
+			},
+		},
+	}
+}
+
+func resourceUnitServiceSpec() *schema.Resource {
+	return &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"ports": &schema.Schema{
+				Type:     schema.TypeList,
+				Required: true,
+				Elem:     resourceUnitServicePort(),
+			},
+			"selector": &schema.Schema{
+				Type:     schema.TypeMap,
+				Optional: true,
+			},
+			"cluster_ip": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"type": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"session_affinity": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+		},
+	}
+}
+
+func resourceUnitServicePort() *schema.Resource {
+	return &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"protocol": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"port": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+			},
+			"node_port": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+			},
+		},
+	}
+}
